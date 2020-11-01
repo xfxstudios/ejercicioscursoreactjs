3120f5554a540c8d9becd06029b11b00b59b5773
Carlos Quintero <info.fxstudios@gmail.com>
2020-11-01 12:01:53 -0500

    02-const-let variables

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,16 +1,1 @@
-//Variables y Constantes
-
-const nombre = 'Carlos';
-let apellido = 'Quintero';
-
-let valorDado = 5;
-
-valorDado = 4;
-
-console.log(nombre, apellido, valorDado);
-
-if(true){
-    const nombre = 'Peter'
-    console.log(nombre)
-}
-console.log(nombre)
+console.log('')

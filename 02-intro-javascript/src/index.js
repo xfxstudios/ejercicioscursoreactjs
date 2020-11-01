07fc5b311d17eaa19a90335d233d18b2839a61e0
Carlos Quintero <info.fxstudios@gmail.com>
2020-11-01 11:59:49 -0500

    02-Variables y Constantes

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//Variables y Constantes
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const nombre = 'Carlos';
+let apellido = 'Quintero';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let valorDado = 5;
+
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+if(true){
+    const nombre = 'Peter'
+    console.log(nombre)
+}
+console.log(nombre)
